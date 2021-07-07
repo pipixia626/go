0b4d673f962a4cffac230870ada12266b8eed352
pipixia626 <814151675@qq.com>
2021-07-07 18:01:06 +0800

    添加web和并发url

---
+++ ch1/server2/main.go
@@ -0,0 +1,35 @@
+//输出了同样的内容，但是会对请求的次数进行计算；
+//对URL的请求结果会包含各种URL被访问的总次数
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+//处理程序回显请求URL r的路径部分
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path=%q\n", r.URL.Path)
+
+}
+
+//回显到目前为止调用的次数
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
